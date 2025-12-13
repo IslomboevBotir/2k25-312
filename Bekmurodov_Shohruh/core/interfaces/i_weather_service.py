d39fac2bcd46ee306e380a1a39f17be235e9739e
Ogabek Normatov <ogabeknormatov@MacBook-Air-Ogabek.local>
2025-12-13 17:45:16 +0500

    The task has been done

--- Bekmurodov_Shohruh/core/interfaces/i_weather_service.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Tuple
-
-
-class IWeatherService(ABC):
-
-    @abstractmethod
-    def get_city_weather(self, city: str) -> Tuple[int, bool]:
-        raise NotImplementedError
